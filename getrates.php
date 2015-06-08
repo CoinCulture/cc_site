f34cc49b07905a6da49b00b0f4db939d992d44b0
Zach Ramsay <zach@coinculture.info>
2015-06-08 12:25:26 -0400

    init commit, site

---
+++ getrates.php
@@ -0,0 +1,38 @@
+<?php
+
+function get_rate() {
+	$ch = curl_init();
+	$url = "https://bitpay.com/rates/cad";
+	curl_setopt($ch, CURLOPT_URL, $url);
+    	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    	curl_setopt($ch, CURLOPT_HEADER, FALSE);
+   	curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json; charset=utf-8"));
+    	curl_setopt($ch, CURLOPT_HTTPGET, TRUE);
+    	//curl_setopt($ch, CURLOPT_POSTFIELDS, FALSE);
+    	$response = curl_exec($ch);
+//	error_log($response);
+	curl_close($ch);
+	$obj = json_decode($response, TRUE);
+    	return $obj;
+}
+
+$var = get_rate();
+error_log("var ".var_export($var, true));
+
+function get_price($p) {
+	$var = get_rate();
+	$var = $var['data']['rate'];
+	error_log("wtf ".var_export($var, true));
+	$var = round($var, 2);
+	$buy = $var * 1.06;
+	$sell = $var * 0.97;
+	$buy = round($buy, 2);
+	$sell = round($sell, 2);
+	if($p === 'B') {return $buy;}
+	if($p === 'S') {return $sell;}
+}
+
+//$var1 = get_price();
+//error_log("buy ".var_export($var1, true));
+
+?>
---
+++ index.php
@@ -0,0 +1,285 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="author" content="CoinCulture: Education and Consultation">
+    <meta name="description" content="CoinCulture is a product of the Bitcoin revolution. WeÕve emerged to help you understand this new technology and to integrate its remarkable potential into your digital life.">
+    <meta name="robots" content="all">
+    
+    <title>Home | CoinCulture</title>
+    
+    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="stylesheet" href="css/foundation.css"/>
+    <link rel="stylesheet" href="css/style.css"/>
+    
+    <script src="js/vendor/modernizr.js"></script>
+    
+    <script>
+     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+   
+     ga('create', 'UA-48302191-4', 'coinculture.info');
+     ga('send', 'pageview');
+   
+   </script>
+  </head>
+  
+  <body>
+  
+  <div class="contain-to-grid fixed columns">
+    <nav class="top-bar" data-topbar>
+      <ul class="title-area">
+	<li class="name">
+	  <a href="#intro" class="internalScroll"><img src="img/coinculture.png" width="20%" alt="The CoinCulture logo"/></a>
+	</li>
+	 <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+	<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+      </ul>
+    
+      <section class="top-bar-section">
+	<!-- Right Nav Section -->
+	<ul class="right">
+	  <li class="active"><a href="#logo" class="internalScroll">CoinCulture</a></li>
+	  <li class="active"><a href="#convenience" class="internalScroll">Bitcoin</a></li>
+	  <li class="active"><a href="#community" class="internalScroll">Press</a></li>
+	  <li class="active"><a href="#collaboration" class="internalScroll">Services</a></li>
+	  <li class="active"><a href="#footer" class="internalScroll">Contact</a></li>
+	</ul>
+    </nav>
+  </div>
+  
+  <!----------------------------------------------------------------------------------------------------------------->
+  
+  <div class="row" id="intro">
+    <div id="background">
+      <div class="small-12 medium-centered-10 large-centered large-10 columns text-center">
+          <h2>Finance can be better.<br> Let's make it happen together.</h2>
+
+	<p>
+    <strong>&bull; </strong> Accessible digital currency solutions. <br>
+    <strong>&bull; </strong> Web technologies for simplifying financial services. <br>
+    <strong>&bull; </strong> Revenue sharing based collaboration.
+	</p>
+<!--    <h2>
+    Learn more about what we do at <a href="http://blog.coinculture.info" style="color:black">blog.coinculture.info</a>
+    </h2> -->
+      </div>
+    </div>
+  </div>
+
+
+  <!--
+  <div class="row" id="event">
+    <div class="small-12 medium-centered text-center">
+        <h3>Upcoming Event!<h3>
+      <img src="img/what_is_bitcoin_flyer.png"  style="height:500px; width:auto;" >
+      <h5>The event is pay-what-you-can, but for $10 we will give you $5 worth of bitcoin</h5>
+    </div>
+  </div>
+  -->
+
+  <div class="row" id="logo">
+    <div class="small-12 medium-centered text-center">
+      <img src="img/coinculture.png" alt="The CoinCulture Logo"/>
+    </div>
+  </div>
+  
+
+  <!-----process------------------------------------------------------------------------------------------------------------>
+  
+  <div class="row" id="process">
+    <div class="small-centered small-9 medium-12 large-9 text-center columns">
+      <ul class="medium-block-grid-3">
+	<li>
+	  <h3 class="heading"><a href="#convenience" class="internalScroll">Convenience</a></h3>
+	  <img src="img/atm.png" width="90%" alt="Flat Icon of a Debit Transaction">
+      <p><strong>&bull; </strong> Buy & Sell Bitcoin where  you get your Dollars, Euros and Yen. <br>
+      <strong> &bull; </strong>On demand cash ATMs at your event or location.
+	  </p>
+	</li>
+	
+	<li>
+	  <h3 class="heading"><a href="#community" class="internalScroll">Community</a></h3>
+	  <img src="img/community.png" width="90%" alt="Flat Icon of Plant Representing Growing Community">
+      <p><strong>&bull; </strong> Meetups and seminars on digital currencies and alternative economies. <br>
+      <strong>&bull; </strong> Open-source platforms for resource management.
+	</li>
+	
+	<li>
+	  <h3 class="heading"><a href="#collaboration" class="internalScroll">Collaboration</a></h3>
+	  <img src="img/collab.png" width="90%" alt="Flat Icon of people talking and brainstorming">
+      <p><strong>&bull; </strong> Software development with ongoing suport. </br>
+      <strong>&bull; </strong> Flexible pricing and barter models.
+	</li>
+      </ul>
+    </div>
+  </div>
+
+
+  <!------mail--------------------------------------->
+  
+  <div style="width:60%" class="row" id="mailing">
+    <div class="small-12 medium-centered text-center">
+        <br>
+        <!-- Begin MailChimp Signup Form -->
+        <link href="//cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet" type="text/css">
+        <style type="text/css">
+    #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+        /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+               We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+        </style>
+        <div id="mc_embed_signup">
+            <form action="//coinculture.us10.list-manage.com/subscribe/post?u=23c3f3a0a0413796608d05e1f&amp;id=709c042d66" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                    <div id="mc_embed_signup_scroll">
+                            <h2> Weekly meetups have evolved into periodic info sessions.</h2>
+                            <h2>For more info and regular updates, subscribe to our mailing list.</h2>
+                            <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+                            <div class="mc-field-group">
+                                    <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+                                    </label>
+                                        <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                                    </div>
+                                    <div class="mc-field-group">
+                                            <label for="mce-FNAME">First Name </label>
+                                                <input type="text" value="" name="FNAME" class="" id="mce-FNAME">
+                                            </div>
+                                            <div class="mc-field-group">
+                                                    <label for="mce-LNAME">Last Name </label>
+                                                        <input type="text" value="" name="LNAME" class="" id="mce-LNAME">
+                                                    </div>
+                                                        <div id="mce-responses" class="clear">
+                                                                    <div class="response" id="mce-error-response" style="display:none"></div>
+                                                                            <div class="response" id="mce-success-response" style="display:none"></div>
+                                                                                </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                                                                                    <div style="position: absolute; left: -5000px;"><input type="text" name="b_23c3f3a0a0413796608d05e1f_709c042d66" tabindex="-1" value=""></div>
+                                                                                        <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                                                                                            </div>
+                                                                                        </form>
+                                                                                    </div>
+                                                                                    <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+                                                                                    <!--End mc_embed_signup-->
+    </div>
+  </div>
+    </div>
+  </div>
+
+
+  <!-----convenience------------------------------------------------------------------------------------------------------------>
+  
+  <div class="row content clearfix" id="convenience">
+  <?php include "getrates.php";
+ $buy = get_price('B'); $sell = get_price('S');?>
+    <div id="content" class="small-centered medium-12 large-centered large-8 columns">
+      <div class="small-0 medium-3 large-4 columns">
+	<img src="img/atm.png" alt="Flat icon of ATM transaction.">
+      </div>
+      <div class="small-12 medium-9 large-8 columns">
+	<h3 class="heading small-text-left large-text-left">Buy & Sell Bitcoin in Guelph</h3>
+	<p>Bitcoin ATM's are the simplest and safest way to buy bitcoin. All you need is a cellphone.
+    </p>
+	  <ul>
+	    <h6>Locations:</h6>
+	    <li><a href="http://global-currency.on.ca/" target="_blank">Global Currency Services</a>, 1027 Gordon St, Guelph, ON</li>
+	    <li>More on the way!</li>
+	  </ul>
+      <h6>For help or assistance with your first purchase, contact us at support@coinculture.info</h6><br>
+	<h3>Live Rates: Price Per BTC</h3>
+	<h4>Buy Bitcoin at: <?php echo '$ '.$buy.' CAD' ?> </h4>
+	<h4>Sell Bitcoin at: <?php echo '$ '.$sell.' CAD' ?> </h4>
+      </div>
+    </div>
+  </div>
+
+
+  <!----------------------------------------------------------------------------------------------------------------->
+  
+  <div class="row content " id="community">
+    <div id="content" class="small-centered medium-12 large-centered large-10 columns">
+      <div class="small-0 medium-3 large-4 columns">
+	    <img src="img/coincultureATM.jpg" alt="Flat icon of ATM transaction.">
+      </div>
+      <div class="small-12 medium-9 large-8 columns">
+	<h3 class="heading small-text-left large-text-left">Press</h3>
+	  <ul>
+        <li>01/19/2015 <a href="http://www.guelphmercury.com/news-story/5265489-guelph-bitcoin-backers-remain-hopeful-about-local-prospects/"> Guelph Bitcoin backers remain optimistic </a></li>
+        <li>06/18/2014 <a href="http://www.guelphmercury.com/news-story/4584672-bitcoin-atms-open-door-for-digital-currency-in-guelph/"> Bitcoin ATMs open door for digital currency in Guelph </a></li>
+	  </ul>
+      </div>
+    </div>
+  </div>
+  
+
+  <!-----contact------------------------------------------------------------------------------------------------------------>
+  
+  <div class="row content clearfix" id="collaboration">
+    <div id="content" class="small-centered medium-12 large-centered large-8 columns">
+      <div class="small-0 medium-3 large-4 columns">
+	<img src="img/collab.png" alt="Flat icon of ATM transaction.">
+      </div>
+      <div class="small-12 medium-9 large-8 columns">
+	<h3 class="heading small-text-left large-text-left">Contact us for:</h3>
+	  <ul>
+          <li>Immediate support regarding a bitcoin transaction at one of our BTMs.</li>
+          <li>Information on bitcoin and digital currencies, including underlying technology, how to buy & sell, and how to incorporate them into your business.</li>
+          <li>Details on upcoming meetups and seminars or to book us for your own.</li>
+          <li>Web development for financial services technologies.</li>
+          <li>Cash ATM bookings for your event or location.</li>
+	  </ul>
+      <h6>support@coinculture.info</h6>
+      </div>
+    </div>
+  </div>
+
+
+
+  <!----------------------------------------------------------------------------------------------------------------->
+  
+
+  <div class="row content" id="footer">
+    <div class="small-centered small-12 medium-8 large-centered large-6 columns small-text-right">
+      <div id="content">
+	<p>Ethan Buchman
+	  <a href="https://www.linkedin.com/pub/ethan-buchman/44/349/10b" target="_blank"><img src="img/linkedin.png" alt="LinkedIn Logo"/></a>
+	  <a href="https://twitter.com/buchmanster" target="_blank"><img src="img/twitter.png" alt="Twitter Logo"/></a>
+	</p>
+	<p>Zach Ramsay
+	  <a href="https://www.linkedin.com/profile/view?id=336252652&authType=name&authToken=HOgo&trk=prof-sb-browse_map-name" target="_blank"><img src="img/linkedin.png" alt="LinkedIn Logo"/></a>
+	  <a href="https://twitter.com/coinculture" target="_blank"><img src="img/twitter.png" alt="Twitter Logo"/></a>
+	</p>
+	<p>Vlad Zamfir
+	  <a href="https://twitter.com/vladzamfir" target="_blank"><img src="img/twitter.png" alt="Twitter Logo"/></a>
+	</p>
+	<p id="jordan" class="small-text-center">CoinCulture &copy 2014-2015 | site by <a href="http://www.jordanbibla.com" target="_blank">Jordan Bibla</a></p>
+    <p id="jordan" class="small-text-center"><a href="mailto:support@coinculture.info">support@coinculture.info</a></p>
+      </div>
+    </div>
+  </div>
+  
+<!---------------javscript ya'll--------------------------------->
+
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script src="js/foundation/foundation.topbar.js"></script>
+    
+    <script>
+      $(document).foundation();
+    </script>
+
+    <script>
+      $(document).ready(function(){
+      
+	//alert("hello")
+	  
+	  $('a.internalScroll').bind('click',function(event){
+	    var $anchor = $(this);
+   
+	  $('html, body').stop().animate({
+	      scrollTop: $($anchor.attr('href')).offset().top
+	  }, 700);
+	});
+      });
+    </script>
+    
+  </body>
+</html>
