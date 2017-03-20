5377c76bb5076a92c3258f82f45bc7919977af77
Ethan Buchman <ethan@coinculture.info>
2017-03-20 19:34:37 -0400

    remove php

--- site/hook.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-// receive webhook from github and pull
-// TODO: authenticate the payload
-shell_exec( 'cd /coinculture/ && git fetch -a origin && git reset --hard origin/master' );
-
-?>
